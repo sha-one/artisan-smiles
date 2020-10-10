129f9eb3e4c5a50163b3c57289989082c4ab532d
shaone <sean@shaone.com>
2020-10-10 15:58:04 +0100

    Add github pages

---
+++ scripts/gh-pages.js
@@ -0,0 +1,16 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish(
+    '__sapper__/export/artisan-smiles',
+    {
+        branch: 'master',
+        repo: 'https://github.com/sha-one/artisan-smiles.git',
+        user: {
+            name: 'sha-one',
+            email: 'web@sccreative.co.uk'
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
