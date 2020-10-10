5973c789a5e0c9b23e0316c87874c3c0fe1dbe2f
shaone <sean@shaone.com>
2020-10-10 16:24:45 +0100

    Update ghpages setup

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,7 +1,7 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish(
-    '__sapper__/export/artisan-smiles',
+    '__sapper__/export',
     {
         branch: 'master',
         repo: 'https://github.com/sha-one/artisan-smiles.git',
