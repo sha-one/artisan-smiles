c3d3737add6a605df7492cee720eb9ae19ef9c24
shaone <sean@shaone.com>
2020-10-29 09:20:14 +0000

    update deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,16 @@
-const ghpages = require('gh-pages');
+const ghpages = require('gh-pages')
 
 ghpages.publish(
-    '__sapper__/export/artisan-smiles',
-    {
-        branch: 'master',
-        repo: 'https://github.com/sha-one/artisan-smiles.git',
-        user: {
-            name: 'sha-one',
-            email: 'web@sccreative.co.uk'
-        }
-    },
-    () => {
-        console.log('Deploy Complete!')
+  '__sapper__/export',
+  {
+    branch: 'master',
+    repo: 'https://github.com/sha-one/artisan-smiles.git',
+    user: {
+      name: 'sha-one',
+      email: 'web@sccreative.co.uk'
     }
+  },
+  () => {
+    console.log('Deploy Complete!')
+  }
 )
